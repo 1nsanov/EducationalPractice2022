e34aff9c54bc3415ea514e81bfa7ff47a3d56919
v.bezunov <v.bezunov@tirscript.com>
2022-04-04 10:38:20 +0300

    Добавьте файлы проекта.

---
+++ Task_04_04/Point.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_04_04
+{
+    public class Point
+    {
+        private int X { get; set; }
+        private int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"({X}; {Y})";
+        }
+    }
+}
