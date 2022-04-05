259e307075100f9a8cd422364dbc034cb6f4677d
v.bezunov <v.bezunov@tirscript.com>
2022-04-05 14:59:18 +0300

    Update class Help

--- Helper/Help.cs
+++ Helper/Help.cs
@@ -4,22 +4,18 @@
     {
         public static int ParseInt()
         {
-            string? value;
             while (true)
             {
-                value = Console.ReadLine();
-                if (int.TryParse(value, out int val)) { return val; }
+                if (int.TryParse(Console.ReadLine(), out int value)) { return value; }
                 Console.WriteLine("Неверный формат. Повторите попытку.");
             }
         }
 
         public static double ParseDouble()
         {
-            string? value;
             while (true)
             {
-                value = Console.ReadLine();
-                if (double.TryParse(value, out double val)) { return val; }
+                if (double.TryParse(Console.ReadLine(), out double value)) { return value; }
                 Console.WriteLine("Неверный формат. Повторите попытку.");
             }
         }
