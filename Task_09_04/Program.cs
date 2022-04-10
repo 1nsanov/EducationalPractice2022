e94047adfc68145541f0da1ffaad5cbc29a83854
1nsanov <victorbezunov@gmail.com>
2022-04-10 11:09:11 +0300

    update

--- Task_09_04/Program.cs
+++ Task_09_04/Program.cs
@@ -18,31 +18,31 @@ class Program
         switch (Console.ReadKey().Key)
         {
             case ConsoleKey.Q:
-                dataSource.WrapperSwitchAction(OutputRoutesBuses);
+                dataSource.WrapperSwitchAction(_listRoutesBuses.OutputRoutesBuses);
                 SelectorUISwitch();
                 break;
             case ConsoleKey.W:
-                dataSource.WrapperSwitchAction(WriteToFile);
+                dataSource.WrapperSwitchAction(_listRoutesBuses.WriteToFile);
                 SelectorUISwitch();
                 break;
             case ConsoleKey.E:
-                dataSource.WrapperSwitchAction(ReadFromFile);
+                dataSource.WrapperSwitchAction(_listRoutesBuses.ReadFromFile);
                 SelectorUISwitch();
                 break;
             case ConsoleKey.R:
-                dataSource.WrapperSwitchAction(GetCountUrabanBus);
+                dataSource.WrapperSwitchAction(_listRoutesBuses.GetCountUrabanBus);
                 SelectorUISwitch();
                 break;
             case ConsoleKey.T:
-                dataSource.WrapperSwitchAction(GetRouteById);
+                dataSource.WrapperSwitchAction(_listRoutesBuses.GetRouteById);
                 SelectorUISwitch();
                 break;
             case ConsoleKey.Y:
-                dataSource.WrapperSwitchAction(GetBusesIdByCity);
+                dataSource.WrapperSwitchAction(_listRoutesBuses.GetBusesIdByCity);
                 SelectorUISwitch();
                 break;
             case ConsoleKey.U:
-                dataSource.WrapperSwitchAction(GetRoutesByBusBaseId);
+                dataSource.WrapperSwitchAction(_listRoutesBuses.GetRoutesByBusBaseId);
                 SelectorUISwitch();
                 break;
             case ConsoleKey.I:
@@ -52,40 +52,5 @@ class Program
                 SelectorUISwitch();
                 break;
         }
-    }
-
-    private static void OutputRoutesBuses()
-    {
-        _listRoutesBuses.OutputRoutesBuses();
-    }
-
-    private static void WriteToFile()
-    {
-        _listRoutesBuses.WriteToFile();
-    }
-
-    private static void ReadFromFile()
-    {
-        _listRoutesBuses.ReadFromFile();
-    }
-
-    private static void GetCountUrabanBus()
-    {
-        _listRoutesBuses.GetCountUrabanBus();
-    }
-
-    private static void GetRouteById()
-    {
-        _listRoutesBuses.GetRouteById();
-    }
-
-    private static void GetBusesIdByCity()
-    {
-        _listRoutesBuses.GetBusesIdByCity();
-    }
-
-    private static void GetRoutesByBusBaseId()
-    {
-        _listRoutesBuses.GetRoutesByBusBaseId();
     }
 }
