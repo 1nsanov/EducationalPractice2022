7e64b2f5c6bb601d5af15925c43e7ee33d5f0b36
1nsanov <victorbezunov@gmail.com>
2022-04-12 21:17:21 +0300

    Add task 12.04.2022

--- Task_12_04/Form1.Designer.cs
+++ Task_12_04/Form1.Designer.cs
@@ -3,14 +3,14 @@
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Освободить все используемые ресурсы.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,28 +20,158 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Код, автоматически созданный конструктором форм Windows
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.startBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buildGraphic = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.endBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.stepBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.selectFont = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
             this.SuspendLayout();
+            // 
+            // chart
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart.Legends.Add(legend3);
+            this.chart.Location = new System.Drawing.Point(11, 85);
+            this.chart.Name = "chart";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart.Series.Add(series3);
+            this.chart.Size = new System.Drawing.Size(447, 240);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart";
+            // 
+            // startBox
+            // 
+            this.startBox.Location = new System.Drawing.Point(11, 24);
+            this.startBox.Name = "startBox";
+            this.startBox.Size = new System.Drawing.Size(60, 20);
+            this.startBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Start X";
+            // 
+            // buildGraphic
+            // 
+            this.buildGraphic.Location = new System.Drawing.Point(12, 50);
+            this.buildGraphic.Name = "buildGraphic";
+            this.buildGraphic.Size = new System.Drawing.Size(210, 29);
+            this.buildGraphic.TabIndex = 3;
+            this.buildGraphic.Text = "Build Graphic";
+            this.buildGraphic.UseVisualStyleBackColor = true;
+            this.buildGraphic.Click += new System.EventHandler(this.buildGraphic_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "End X";
+            // 
+            // endBox
+            // 
+            this.endBox.Location = new System.Drawing.Point(87, 24);
+            this.endBox.Name = "endBox";
+            this.endBox.Size = new System.Drawing.Size(60, 20);
+            this.endBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(176, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Step";
+            // 
+            // stepBox
+            // 
+            this.stepBox.Location = new System.Drawing.Point(162, 24);
+            this.stepBox.Name = "stepBox";
+            this.stepBox.Size = new System.Drawing.Size(60, 20);
+            this.stepBox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(238, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 8;
+            // 
+            // selectFont
+            // 
+            this.selectFont.Location = new System.Drawing.Point(248, 50);
+            this.selectFont.Name = "selectFont";
+            this.selectFont.Size = new System.Drawing.Size(210, 29);
+            this.selectFont.TabIndex = 9;
+            this.selectFont.Text = "Select font";
+            this.selectFont.UseVisualStyleBackColor = true;
+            this.selectFont.Click += new System.EventHandler(this.selectFont_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1171, 892);
+            this.ClientSize = new System.Drawing.Size(467, 337);
+            this.Controls.Add(this.selectFont);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.stepBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.endBox);
+            this.Controls.Add(this.buildGraphic);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startBox);
+            this.Controls.Add(this.chart);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.TextBox startBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buildGraphic;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox endBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox stepBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button selectFont;
     }
 }
+
--- Task_12_04/Form1.cs
+++ Task_12_04/Form1.cs
@@ -1,7 +1,12 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
 namespace Task_12_04
 {
     public partial class Form1 : Form
     {
+        private static Random rnd = new Random();
         public Form1()
         {
             InitializeComponent();
@@ -9,7 +14,61 @@ namespace Task_12_04
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            chart.ChartAreas.Add(new ChartArea("Graphic"));
+        }
 
+        private void buildGraphic_Click(object sender, EventArgs e)
+        {
+            InitChart(out Series series);
+            BuildGraphic(series);
+        }
+
+        private void InitChart(out Series series)
+        {
+            chart.Series.Clear();
+            chart.ChartAreas[0].AxisX.MajorGrid.Enabled = true;
+            chart.ChartAreas[0].AxisY.MajorGrid.Enabled = true;
+            series = new Series(Convert.ToString(rnd.Next(0, 1000)));
+            series.ChartType = SeriesChartType.Line;
+            series.ChartArea = "Graphic";
+        }
+
+        private void BuildGraphic(Series series)
+        {
+            var start = Convert.ToDouble(startBox.Text);
+            var end = Convert.ToDouble(endBox.Text);
+            if (start < 0.4) start = 0.4;
+            if(end > 6) end = 6;
+            for (double i = start; i <= end; i += Convert.ToDouble(stepBox.Text))
+            {
+                series.Points.AddXY(i, GetY(i));
+            }
+            label4.Text = $"Graphic: X from {start} to {end}";
+            chart.Series.Add(series);
+        }
+
+        private double GetY(double x)
+        {
+            return Math.Pow(x, Math.Cos(x)) / (Math.Abs(x + Math.Pow(Math.E, x)) + Math.Tan(x));
+        }
+
+        private void selectFont_Click(object sender, EventArgs e)
+        {
+            var fontDialog = new FontDialog();
+            if (fontDialog.ShowDialog() == DialogResult.OK) SetFont(fontDialog);
+        } 
+
+        private void SetFont(FontDialog fontDialog)
+        {
+            label1.Font = fontDialog.Font;
+            label2.Font = fontDialog.Font;
+            label3.Font = fontDialog.Font;
+            label4.Font = fontDialog.Font;
+            buildGraphic.Font = fontDialog.Font;
+            selectFont.Font = fontDialog.Font;
+            startBox.Font = fontDialog.Font;
+            endBox.Font = fontDialog.Font;
+            stepBox.Font = fontDialog.Font;
         }
     }
 }
